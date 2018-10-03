b853d247f61349ea1419d694c34607af4833c32b
Syracks1 <36195834+Syracks1@users.noreply.github.com>
2018-10-03 10:58:40 +0200

    Now checks if form is empty

--- inc/poll.php
+++ inc/poll.php
@@ -8,13 +8,17 @@
      // $statement->execute();
      // $employee = $statement->fetchAll(PDO::FETCH_OBJ);
 
+     // Below code hides errors. PLEASE enable during coding
+     // error_reporting(E_ERROR | E_PARSE);
+
      $age = $_POST['age'];
      $buy = $_POST['buy'];
      $like = $_POST['like'];
 
-
-     DB::query('INSERT INTO poll(age, buy, rec) VALUES (:age, :buy, :rec)', array(':age'=>$age, ':buy'=>$buy, ':rec'=>$like));
-     $echo = 'Success!';
+     if($age != null && $buy != null && $like !=null){
+          DB::query('INSERT INTO poll(age, buy, rec) VALUES (:age, :buy, :rec)', array(':age'=>$age, ':buy'=>$buy, ':rec'=>$like));
+          $echo = 'Success!';
+     }
 
      echo $age . '<br />' . $buy . '<br />' . $like;
 ?>
