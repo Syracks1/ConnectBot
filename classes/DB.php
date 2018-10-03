ca65e3c5587cec27cac15d53c213febc898b1edd
Syracks1 <36195834+Syracks1@users.noreply.github.com>
2018-10-03 10:55:09 +0200

    Saving poll results to DB works now

--- classes/DB.php
+++ classes/DB.php
@@ -2,7 +2,7 @@
 class DB {
 
         private static function connect() {
-                $pdo = new PDO('mysql:host=127.0.0.1;dbname=ToolsForEver;charset=utf8', 'root', '');
+                $pdo = new PDO('mysql:host=127.0.0.1;dbname=connectbot;charset=utf8', 'root', '');
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $pdo;
         }
--- inc/poll.php
+++ inc/poll.php
@@ -1,8 +1,20 @@
 <?php
      include '../html/poll.html';
+     include '../classes/DB.php';
+
+     // $pdo = new PDO('mysql:host=127.0.0.1;dbname=connectbot;charset=utf8', 'root', '');
+     // $sql = 'SELECT * FROM poll';
+     // $statement = $pdo->prepare($sql);
+     // $statement->execute();
+     // $employee = $statement->fetchAll(PDO::FETCH_OBJ);
 
      $age = $_POST['age'];
      $buy = $_POST['buy'];
      $like = $_POST['like'];
+
+
+     DB::query('INSERT INTO poll(age, buy, rec) VALUES (:age, :buy, :rec)', array(':age'=>$age, ':buy'=>$buy, ':rec'=>$like));
+     $echo = 'Success!';
+
      echo $age . '<br />' . $buy . '<br />' . $like;
 ?>
